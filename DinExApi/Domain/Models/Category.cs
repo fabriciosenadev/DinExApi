91b6524117fea975c3923777f0aecc1472dc9f70
Fabricio Sena <fabricior.sena@outlook.com>
2021-03-26 09:58:16 -0300

    feat: add category model

---
+++ DinExApi/Domain/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinExApi.Domain.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Applicable { get; set; } //TODO: migrar para enum
+        public string IsCustom { get; set; } //TODO: migrar para enum
+        //TODO: Verificar necessidade de criar props de controle: CreatedBY, CreatedAt, UpdatedAt e DeletedAt
+    }
+}
