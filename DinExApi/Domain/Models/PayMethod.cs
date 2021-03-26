b7e3186b3f01b216870148434437050f43c30d73
Fabricio Sena <fabricior.sena@outlook.com>
2021-03-26 09:50:34 -0300

    initial commit

---
+++ DinExApi/Domain/Models/PayMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinExApi.Domain.Models
+{
+    public class PayMethod
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //TODO: Migrar prop Applicable para enum
+        public string Applicable { get; set; }
+        //TODO: Verificar necessidade de criar props de controle: CreatedBY, CreatedAt, UpdatedAt e DeletedAt
+    }
+}
---
+++ DinExApi/Infrastructure/DB/Data/DinExContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DinExApi.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DinExApi.Infrastructure.DB.Data
+{
+    public class DinExContext: DbContext
+    {
+        public DinExContext(DbContextOptions<DinExContext> options) : base(options)
+        {
+        }
+
+        public DbSet<PayMethod> PayMethod { get; set; }
+    }
+}
