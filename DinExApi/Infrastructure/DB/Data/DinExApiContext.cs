adfe07b53fda2d58f674f2e606a1f9641c61827e
Fabricio Sena <fabricior.sena@outlook.com>
2021-04-08 21:31:12 -0300

    Ajusta nomenclatura do context para o padrão.

---
+++ DinExApi/Infrastructure/DB/Data/DinExApiContext.cs
@@ -0,0 +1,19 @@
+﻿using DinExApi.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinExApi.Infrastructure.DB.Data
+{
+    public class DinExApiContext : DbContext
+    {
+        public DbSet<Category> Category { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=DinExDB.sqlite");
+        }
+    }
+}
