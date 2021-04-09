4c2b288c011e4927be94108a266b4fbf990dc622
Fabricio Sena <fabricior.sena@outlook.com>
2021-04-09 20:39:10 -0300

    Adiciona modelo de usuário.

---
+++ DinExApi/Domain/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinExApi.Domain.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime DeletedAt { get; set; }
+    }
+}
