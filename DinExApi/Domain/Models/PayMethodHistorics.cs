8a0df4719f548fc8eacb36085c3b7760b8b5975c
Fabricio Sena <fabricior.sena@outlook.com>
2021-04-09 22:44:37 -0300

    Adiciona Model de relacionamento entre metodos de pagamento e historicos.

---
+++ DinExApi/Domain/Models/PayMethodHistorics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinExApi.Domain.Models
+{
+    public class PayMethodHistorics
+    {
+        public int Id { get; set; }
+        public PayMethod PayMethod { get; set; }
+        public Launch Launch { get; set; }
+        public Category Category { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime DeletedAt { get; set; }
+    }
+}
