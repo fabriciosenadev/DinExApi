3b122f480d0c7e6c5b1b07fbf715f3e0c9eb5e92
Fabricio Sena <fabricior.sena@outlook.com>
2021-04-09 22:45:02 -0300

    Adiciona Model de agendamento de lançamentos.

---
+++ DinExApi/Domain/Models/ScheduledLaunches.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinExApi.Domain.Models
+{
+    public class ScheduledLaunches
+    {
+        public int Id { get; set; }
+        public Launch Launch { get; set; }
+        public string NextMonth { get; set; }
+        public User CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime DeletedAt { get; set; }
+    }
+}
