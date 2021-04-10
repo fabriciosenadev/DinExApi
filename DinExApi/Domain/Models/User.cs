0ca8da7ee50231181290c6987a59139f049406f2
Fabricio Sena <fabricior.sena@outlook.com>
2021-04-09 23:18:59 -0300

    Ajusta propriedade

--- DinExApi/Domain/Models/User.cs
+++ DinExApi/Domain/Models/User.cs
@@ -8,7 +8,7 @@ namespace DinExApi.Domain.Models
     public class User
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string FullName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public DateTime CreatedAt { get; set; }
