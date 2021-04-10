4fed25c0a863e3a66368b7ce9ca68db523126a2b
Fabricio Sena <fabricior.sena@outlook.com>
2021-04-09 22:46:20 -0300

    Adiciona Model de relacionamento entre valor disponível e usuário.

---
+++ DinExApi/Domain/Models/UserAmounts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinExApi.Domain.Models
+{
+    public class UserAmounts
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public decimal AmountAvailable { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
