9c3303f27a614c4f01c992b70492be8e68c7bd27
Fabricio Sena <fabricior.sena@outlook.com>
2021-04-09 22:44:04 -0300

    Adiciona model de relacionamento entre categoria e usuario.

---
+++ DinExApi/Domain/Models/CategoryUsers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinExApi.Domain.Models
+{
+    public class CategoryUsers
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public Category Category { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime DeletedAt { get; set; }
+    }
+}
