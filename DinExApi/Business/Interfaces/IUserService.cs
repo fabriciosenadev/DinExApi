c7125445bdef8f63be1e5755da6fde0d1c76650a
Fabricio Sena <fabricior.sena@outlook.com>
2021-04-12 01:22:04 -0300

    Novo: Adiciona utilização da entidade User

---
+++ DinExApi/Business/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using DinExApi.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinExApi.Business.Interfaces
+{
+    public interface IUserService
+    {
+        Task<int> AddAsync(User user);
+        Task UpdateAsync(int userId, string password);
+        Task DeleteAsync(int userId);
+        Task<User> FindByIdAsync(int userId);
+    }
+}
---
+++ DinExApi/Business/Services/UserService.cs
@@ -0,0 +1,66 @@
+﻿using DinExApi.Business.Interfaces;
+using DinExApi.Domain.Models;
+using DinExApi.Persistence.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinExApi.Business.Services
+{
+    public class UserService : BaseServices<User>, IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<int> AddAsync(User user)
+        {
+            user.CreatedAt = DateTime.Now;
+            if (Validate(user))
+                return await _userRepository.AddAsync(user);
+
+            return 0;
+        }
+
+        public async Task DeleteAsync(int userId)
+        {
+            User user = await FindByIdAsync(userId);
+            await _userRepository.DeleteAsync(user);
+        }
+
+        public async Task UpdateAsync(int userId, string password)
+        {
+            User userToUpdate = await FindByIdAsync(userId);
+            await _userRepository.UpdateAsync(userToUpdate);
+        }
+
+        public async Task<User> FindByIdAsync(int userId)
+        {
+            return await _userRepository.FindByIdAsync(userId);
+        }
+        
+        private async Task<User> FindByEmailAsync(string email)
+        {
+            return await _userRepository.FindByEmailAsync(email);
+        }
+
+        public override bool Validate(User entity)
+        {
+            var user = FindByEmailAsync(entity.Email);
+            if (user != null) return false;
+
+            return true;
+        }
+
+        public override bool Validate(User entity, int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
