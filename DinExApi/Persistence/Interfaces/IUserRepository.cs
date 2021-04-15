eade8179ae95354e4e8da70b443ed0855b624faf
Fabricio Sena <fabricior.sena@outlook.com>
2021-04-14 23:13:39 -0300

    Add repository de usuario na camada de persistencia.

---
+++ DinExApi/Persistence/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using DinExApi.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinExApi.Persistence.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task AddAsync(User user);
+        Task<User> GetUser(int userId);
+    }
+}
---
+++ DinExApi/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using DinExApi.Domain.Models;
+using DinExApi.Infrastructure.DB.Data;
+using DinExApi.Persistence.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinExApi.Persistence.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DinExApiContext _context;
+
+        public UserRepository(DinExApiContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<User> GetUser(int userId)
+        {
+            return await _context.Users.FindAsync(userId); ;
+        }
+    }
+}
