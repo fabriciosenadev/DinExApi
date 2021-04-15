7926ef524cc50010bf7f83a395ae14a71b93eba8
Fabricio Sena <fabricior.sena@outlook.com>
2021-04-14 23:13:06 -0300

    Add serviços de usuario na camada business.

---
+++ DinExApi/Business/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using DinExApi.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinExApi.Business.Interfaces
+{
+    public interface IUserService
+    {
+        Task AddAsync(User user);
+        Task<User> GetUser(int id);
+    }
+}
---
+++ DinExApi/Business/Services/UserService.cs
@@ -0,0 +1,31 @@
+﻿using DinExApi.Business.Interfaces;
+using DinExApi.Domain.Models;
+using DinExApi.Infrastructure.DB.Data;
+using DinExApi.Persistence.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinExApi.Business.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task AddAsync(User user)
+        {
+            await _userRepository.AddAsync(user);
+        }
+
+        public async Task<User> GetUser(int userId)
+        {
+            return await _userRepository.GetUser(userId);
+        }
+    }
+}
