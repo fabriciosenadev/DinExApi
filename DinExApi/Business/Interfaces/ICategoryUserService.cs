f7f4597235ee801b1110fc0d3cc10ea3836e219b
Fabricio Sena <fabricior.sena@outlook.com>
2021-04-18 19:18:04 -0300

    Adiciona serviço de relacionamento entre categoria e usuario.

--- DinExApi/Business/Interfaces/ICategoryUserService.cs
+++ DinExApi/Business/Interfaces/ICategoryUserService.cs
@@ -1,4 +1,5 @@
 ﻿using DinExApi.Domain.Models;
+using DinExApi.Infrastructure.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,6 @@ namespace DinExApi.Business.Interfaces
 {
     public interface ICategoryUserService
     {
-        Task<object> AddRelationAsync(int categoryId, int userId);
+        Task<ErrorCode> AddRelationAsync(int categoryId, int userId);
     }
 }
