560cb13eae1d32479165979f626abf06bbeed6b9
Fabricio Sena <fabricior.sena@outlook.com>
2021-04-28 23:20:01 -0300

    Ajuste na estrutura do serviço de usuário

--- DinExApi/Business/Interfaces/IUserService.cs
+++ DinExApi/Business/Interfaces/IUserService.cs
@@ -1,4 +1,5 @@
 ﻿using DinExApi.Domain.Models;
+using DinExApi.Infrastructure.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,9 @@ namespace DinExApi.Business.Interfaces
 {
     public interface IUserService
     {
-        Task<object> AddAsync(User user);
+        Task<object> ComposeUserCreation(User user);
+        Task<ErrorCode> AddAsync(User user);
         Task<User> FindByIdAsync(int id);
+        Task<User> FindByEmailAsync(string email);
     }
 }
