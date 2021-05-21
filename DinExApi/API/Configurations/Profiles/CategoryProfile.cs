ce2bba8ab1f45f5341095a26fe1fa0310bddc929
Fabricio Sena <fabricior.sena@outlook.com>
2021-05-20 23:42:35 -0300

    Adiciona categoria para perfil de automapper

--- DinExApi/API/Configurations/Profiles/CategoryProfile.cs
+++ DinExApi/API/Configurations/Profiles/CategoryProfile.cs
@@ -1,10 +1,14 @@
-﻿namespace DinExApi.API.Configurations.Profiles
+﻿using AutoMapper;
+using DinExApi.API.DTO;
+using DinExApi.Domain.Models;
+
+namespace DinExApi.API.Configurations.Profiles
 {
-    public class CategoryProfile //: Profile
+    public class CategoryProfile : Profile
     {
         public CategoryProfile()
         {
-
+            CreateMap<Category, CategoryDTO>().ReverseMap();
         }
     }
 }
