1a0798c6c277300cfd91407bc8f1a4be9b4754a6
Fabricio Sena <fabricior.sena@outlook.com>
2021-05-22 13:45:33 -0300

    Adiciona perfil de mapeamento do DTO para o modelo.

---
+++ DinExApi/API/Configurations/Profiles/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DinExApi.API.DTO;
+using DinExApi.Domain.Models;
+
+namespace DinExApi.API.Configurations.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserDTO>().ReverseMap();
+        }
+    }
+}
