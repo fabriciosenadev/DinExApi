48e21a253d8cfe0ddad1093c168ca70bd2b34338
Fabricio Sena <fabricior.sena@outlook.com>
2021-06-16 10:42:29 -0300

    Adiciona validações de propriedades para os dados recebidos.

---
+++ DinExApi/API/Validations/UserDTOValidation.cs
@@ -0,0 +1,28 @@
+﻿using DinExApi.API.DTO;
+using FluentValidation;
+
+namespace DinExApi.API.Validations
+{
+    public class UserDTOValidation : AbstractValidator<UserDTO>
+    {
+        public UserDTOValidation()
+            {
+            RuleFor(u => u.FullName)
+                .MinimumLength(3)
+                .WithName("Nome completo");
+
+            RuleFor(u => u.Email)
+                .NotEmpty()
+                .EmailAddress()
+                .WithName("E-mail");
+
+            RuleFor(u => u.Password)
+                .NotEmpty()
+                .WithName("Senha");
+
+            RuleFor(u => u.VerifyPass)
+                .Equal(u => u.Password)
+                .WithMessage("Senhas devem ser iguais");
+        }
+    }
+}
