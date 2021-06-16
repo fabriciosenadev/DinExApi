65ad32c51f9528642dd7c49d4c7883a1b1d0f9a2
Fabricio Sena <fabricior.sena@outlook.com>
2021-06-16 10:41:07 -0300

    Adiciona filtro para aplicar as validações.

---
+++ DinExApi/API/Validations/DTOStateFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace DinExApi.API.Validations
+{
+    public class DTOStateFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState);
+            }
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            //if (!context.ModelState.IsValid)
+            //{
+            //    context.Result = new BadRequestObjectResult(context.ModelState);
+            //}
+        }
+    }
+}
